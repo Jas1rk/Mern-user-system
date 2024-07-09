30c1a3a997219649fcb34b1dc6e73459a4f756f9
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-07-09 14:00:50 +0530

    ui completed in admin side

---
+++ Frondend/src/Components/Admin/AdminNavbar/Adminnavbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Adminnavbar.css'
+
+const Adminnavbar = () => {
+  return (
+    <header>
+
+    <div className="adminNavBar">
+       <h2>Admin Dashboard</h2>
+       <div className="AdminlogOut">
+        <button>Logout</button>
+       </div>
+    </div>
+    </header>
+  )
+}
+
+export default Adminnavbar
